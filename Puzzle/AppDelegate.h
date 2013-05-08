5499eae697f2575bede68c22bd3b49b3c6f743cf
debugx <debugx@M-M473-Lab-186.local>
2013-05-07 18:42:10 -0700

    Initial Commit

---
+++ Puzzle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Puzzle
+//
+//  Created by debugx on 5/7/13.
+//  Copyright (c) 2013 Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Puzzle/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Puzzle
+//
+//  Created by debugx on 5/7/13.
+//  Copyright (c) 2013 Junior. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
