f153395cdfe461148444a69c599033417214b68c
junior <juniorug@gmail.com>
2013-05-16 21:08:50 -0700

    add Puzzle class to git

---
+++ Puzzle/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  Puzzle
+//
+//  Created by debugx on 5/7/13.
+//  Copyright (c) 2013 Junior. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
---
+++ Tile.h
@@ -0,0 +1,18 @@
+//
+//  Tile.h
+//  Puzzle
+//
+//  Created by debugx on 5/7/13.
+//  Copyright (c) 2013 Junior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tile : NSObject
+
+@property NSInteger position;
+@property NSInteger currentX;
+@property NSInteger currentY;
+@property NSInteger correctX;
+@property NSInteger correctY;
+@end
